b8b6f6e1ff4fd6687b869a918e2ed314f42d985b
Nicolas Albert <nicolas.albert@gmail.com>
2023-06-19 12:48:24 +0200

    copy headers

--- src/routes/[...url]/+server.js
+++ src/routes/[...url]/+server.js
@@ -5,7 +5,10 @@ export async function POST(req) {
     try {
         let body = await req.request.text();
         let { headers, method } = req.request;
-        ['host', 'content-length'].forEach(n => headers.delete(n));
+        // @ts-ignore
+        headers = [...headers.entries()].reduce((m, v) => { m[v[0]] = v[1]; return m; }, {});
+        // @ts-ignore
+        ['host', 'content-length'].forEach(n => delete headers[n]);
         let res = await fetch(`https://api.openai.com/${req.params.url}`, {
             headers, method, body
         });
