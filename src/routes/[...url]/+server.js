cba380d2ecbf285b8c121c072e6901c95afb7df5
Nicolas Albert <nicolas.albert@gmail.com>
2023-06-19 12:23:13 +0200

    try/catch all

--- src/routes/[...url]/+server.js
+++ src/routes/[...url]/+server.js
@@ -2,11 +2,15 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST(req) {
-    let body = await req.request.text();
-    let { headers, method } = req.request;
-    ['host', 'content-length'].forEach(n => headers.delete(n));
-    let res = await fetch(`https://api.openai.com/${req.params.url}`, {
-        headers, method, body
-    });
-    return new Response(await res.text());
+    try {
+        let body = await req.request.text();
+        let { headers, method } = req.request;
+        ['host', 'content-length'].forEach(n => headers.delete(n));
+        let res = await fetch(`https://api.openai.com/${req.params.url}`, {
+            headers, method, body
+        });
+        return new Response(await res.text());
+    } catch (ex) {
+        return new Response("exception: " + ex);
+    }
 }
